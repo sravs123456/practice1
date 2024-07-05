00e1e8e73b84fc5e51c11f82f32cd546d979facb
ADMIN <chennamsettisravya2002@gmail.com>
2024-07-05 08:21:01 +0530

    hi

---
+++ src/main/java/com/neoteric/projectd/ProjectdService.java
@@ -0,0 +1,12 @@
+package com.neoteric.projectd;
+
+import com.neoteric.projectc.Projectc;
+
+public class ProjectdService {
+
+    public String projectd(){
+
+        Projectc projectc=new Projectc();
+        return "projectd";
+    }
+}
